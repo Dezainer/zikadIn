14811c2b22b4218a982c650b5089edc35eee0a7d
allan_horst <allanhorstwillig@gmail.com>
2018-09-15 09:37:43 -0300

    Chaged for tabs

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 export default class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        Home
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				Home
+			</div>
+		)
+	}
 }
--- src/pages/perfil.js
+++ src/pages/perfil.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 export default class Perfil extends React.Component {
-  render() {
-    return (
-      <div>
-        Perfil
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				Perfil
+			</div>
+		)
+	}
 }
